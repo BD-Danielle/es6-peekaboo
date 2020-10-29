e02655396c2a415660152e28682de1d41dab12c1
YilingChen <shutuzi88@gmail.com>
2020-10-29 09:06:24 +0800

    rewrote module

--- js/peekaboo.js
+++ js/peekaboo.js
@@ -1,130 +1,87 @@
 /*
-使用方式如下：
-綁定需被監視的物件target並給予id, 在頁面設置成對的錨點['start', 'end'], 不限組數, 範列如下：
-  $(function () {
-    var peekaboo = new Peekaboo();
-    peekaboo.setArea('target', [['begin', 'end'], ['begin1', 'end1']]);
-
-    window.onscroll = function () {
-      peekaboo.start();
-    }
-  })
-*/
-class Peekaboo {
-  constructor() {
-    if (Peekaboo._instance) {
-      throw new Error('Peekaboo already has an instance!!!');
-    }
-    Peekaboo._instance = this;
+ * ========================================================================
+ * peekaboo 1.0
+ * vertical accordion & content toggle component on the website
+ * YILING CHEN
+ * Copyright 2020, MIT License
+ * How to use it:
+ * window.addEventListener('DOMContentLoaded', function () {
+ *  var peekaboo = new Peekaboo();
+ *  peekaboo.start('#target', [['#begin', '#end']]);
+ * })
+ * ========================================================================
+ */
+(function($, window, document){
+  // if(!$) throw newError('jQuery is not defined');
+  function Peekaboo(){
     this.areas = [];
+    this.visible = [];
+    this.target = null;
+    this.resetNo = null;
   }
 
-  setArea(target, positions) {
-    let positionObj = positions.map(function (pos) {
-      return [this.toObj(pos[0]), this.toObj(pos[1])];
-    }, this);
-    this.areas.push({ target: this.toObj(target), positions: positionObj });
+  // Release module to window
+  window.Peekaboo = Peekaboo;
+
+  // Match the specified selector 
+  Peekaboo.prototype.query = function(ele){
+    return document.querySelectorAll(ele)[0];
   }
 
-  toObj(elem) {
-    let obj;
-    if (typeof (elem) === 'string') {
-      obj = $("#" + elem);
-    } else if (typeof (elem) === 'object') {
-      if (elem instanceof jQuery) {
-        obj = elem;
-      } else {
-        obj = $("#" + elem);
-      }
-    }
-
-    return obj;
-  }
-
-  layout() {
-    let documentHeight = Math.max(
-      document.body.scrollHeight,
-      document.body.clientHeight
-    );
-    let scrolledHeight =
-      window.pageYOffset || //flexible
-      document.documentElement.scrollTop ||
-      document.body.scrollTop;
-    let viewableContent = window.innerHeight; //fixed
-    let viewableBrowser = window.outerHeight; //fixed
-    let browserBarHeight = viewableBrowser - viewableContent; //fixed
-    let unscrolledHeight = documentHeight - scrolledHeight - viewableContent;
+  // Return the detailed size of user screen
+  Peekaboo.prototype.screenSize = function(){
+    // if(!screen) throw newError('screen is not defined');
     return {
-      scrolledHeight: scrolledHeight,
-      viewableBrowser: viewableBrowser,
-      browserBarHeight: browserBarHeight,
-      unscrolledHeight: unscrolledHeight,
-      documentHeight: documentHeight
+      'width': screen.width || screen.availWidth, 
+      'height': screen.height || screen.availHeight
     };
   }
-  div(id) {
-    // if(id == null) return;
-    let documentHeight = this.layout().documentHeight;
-    // divOffsetHeight including padding and border
-    let divOffsetHeight = $(id).outerHeight() || $(id).get(0).offsetHeight;
-    let divOffsetTop =
-      (function () {
-        let myObj = $(id);
-        if (myObj.length) {
-          return myObj.offset().top;
-        }
-      })();
-    let divOffsetBottom =
-      documentHeight - (divOffsetTop + divOffsetHeight);
+  // Return the detailed size of user window
+  Peekaboo.prototype.windowSize = function(){
+    // if(!window) throw newError('window is not defined');
     return {
-      divOffsetTop: divOffsetTop,
-      divOffsetHeight: divOffsetHeight,
-      divOffsetBottom: divOffsetBottom
+      // 'innerWidth': window.innerWidth,    // width without scrollbar-x
+      'innerHeight': window.innerHeight,  // height without scrollbar-y
+      // 'outerWidth': window.outerWidth,    // width with scrollbar-x
+      'outerHeight': window.outerHeight   // height without scrollbar-y
+    };
+  }
+  // Return the detailed size of user document
+  Peekaboo.prototype.documentSize = function(){
+    // if(!document) throw newError('document is not defined');
+    return {
+      'selfHeight': document.body.scrollHeight || document.body.clientHeight || document.documentElement.scrollHeight || document.documentElement.offsetHeight,
+      'loadedHeight': document.documentElement.scrollTop || document.scrollingElement.scrollTop,
+    };
+  }
+  Peekaboo.prototype.objSize = function(ele){
+    return {
+      'selfHeight': this.query(ele).offsetHeight,
+      'offsetTop': this.query(ele).offsetTop
     }
   }
-  start() {
-    this.areas.forEach(function (area) {
-      this.detectVisible(area);
-    }, this);
+  
+  Peekaboo.prototype.start = function(ele, areas){
+    var _this = this;
+    _this.target = this.query(ele);
+    window.addEventListener("scroll", function(){
+      areas.forEach(function(e, i){
+        var winLoadedHeight = _this.documentSize().loadedHeight;
+        var winUnloadedHeight = _this.documentSize().selfHeight - winLoadedHeight - _this.windowSize().innerHeight;
+        var pointA = _this.objSize(e[0]).offsetTop;
+        var pointB = _this.documentSize().selfHeight - (_this.objSize(e[1]).offsetTop + _this.objSize(e[1]).selfHeight);
+        winLoadedHeight >= pointA && pointB <= winUnloadedHeight ? _this.visible.push(1): _this.visible.push(0);
+        
+        _this.resetNo = 0;
+        _this.visible.forEach(function (bool) {_this.resetNo += bool;});
+        _this.resetNo > 0 ? _this.target.style.display = 'block': _this.target.style.display = 'none';
+        
+        if(_this.visible.length >= 2) {_this.visible.shift();}
+      })
+    })
   }
-
-  detectVisible(area) {
-    let cat = area.target.css('display', 'none');
-    let visible = [];
-    area.positions.forEach(function (pos) {
-      let begin = this.div(pos[0]).divOffsetTop;
-      let end = this.div(pos[1]).divOffsetBottom;
-      let scrollBarLocation = this.layout().scrolledHeight;
-      let unscrollBarLocation = this.layout().unscrolledHeight;
-      console.log(`begin: ${begin}, end: ${end}, scrollBarLocation: ${scrollBarLocation}, unscrolledBarLocation: ${unscrollBarLocation}`);
-      switch (document.body.classList.contains('MOBILE') ? 'MOBILE' : document.body.classList.contains('PC') ? 'PC' : '') {
-        case 'PC':
-          if (scrollBarLocation >= begin && end <= unscrollBarLocation) {
-            //true
-            // cat.show();
-            visible.push(1);
-          } else {
-            // cat.hide();
-            visible.push(0);
-          }
-          break;
-        case 'MOBILE':
-          if (scrollBarLocation + 45 >= begin && end <= unscrollBarLocation) {
-            //true
-            // cat.show();
-            visible.push(1);
-          } else {
-            // cat.hide();
-            visible.push(0);
-          }
-          break;
-      }
-    }, this);
-    if (visible.reduce(function (pv, cv) { return pv + cv; }, 0) > 0) {
-      cat.show();
-    } else {
-      cat.hide();
-    }
-  }
-}
-// window.Peekaboo = Peekaboo;
+})(jQuery, window, document);
+// window.addEventListener('DOMContentLoaded', function () {
+//   var peekaboo = new Peekaboo();
+//   peekaboo.start('#target', [['#begin', '#end'], ['#begin1', '#end1']]);
+// })
