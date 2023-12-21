6209b97ac2177396d854f6a6e2971b77a7fe0c3e
YilingChen <shutuzi88@gmail.com>
2023-12-21 21:16:19 +0800

    version updated

--- js/es6.peekaboo2.1.0.js
+++ js/es6.peekaboo2.1.0.js
@@ -1,6 +1,6 @@
 /*
  * ========================================================================
- * Peekaboo 2.0
+ * Peekaboo 2.1.0
  * Toggles visibility of a target element when scrolling to specified areas.
  * YILING CHEN
  * Copyright 2020, MIT License
@@ -13,51 +13,52 @@
  */
 
 class Peekaboo {
-    constructor() {
-        this.areas = [];
-        this.visible = [];
-        this.target = null;
+  static version = "2.1.0";
+  constructor() {
+    this.areas = [];
+    this.visible = [];
+    this.target = null;
+  }
+
+  // Query a single element from the DOM
+  query(selector) {
+    return document.querySelector(selector);
+  }
+
+  // Compute the size of an element
+  getElementSize(selector) {
+    const element = this.query(selector);
+    if (!element) {
+      throw new Error(`Element not found: ${selector}`);
+    }
+    return element.getBoundingClientRect();
+  }
+
+  // Function to handle visibility toggle
+  updateVisibility(ele, areas) {
+    const target = this.query(ele);
+    if (!target) {
+      throw new Error(`Target element not found: ${ele}`);
     }
 
-    // Query a single element from the DOM
-    query(selector) {
-        return document.querySelector(selector);
-    }
+    let isVisible = areas.some(area => {
+      const beginRect = this.getElementSize(area[0]);
+      const endRect = this.getElementSize(area[1]);
+      const viewportHeight = window.innerHeight;
+      const scrollY = window.scrollY;
 
-    // Compute the size of an element
-    getElementSize(selector) {
-        const element = this.query(selector);
-        if (!element) {
-            throw new Error(`Element not found: ${selector}`);
-        }
-        return element.getBoundingClientRect();
-    }
+      return scrollY + viewportHeight >= beginRect.top + scrollY &&
+        scrollY <= endRect.top + scrollY + endRect.height;
+    });
 
-    // Function to handle visibility toggle
-    updateVisibility(ele, areas) {
-        const target = this.query(ele);
-        if (!target) {
-            throw new Error(`Target element not found: ${ele}`);
-        }
+    target.style.display = isVisible ? 'block' : 'none';
+  }
 
-        let isVisible = areas.some(area => {
-            const beginRect = this.getElementSize(area[0]);
-            const endRect = this.getElementSize(area[1]);
-            const viewportHeight = window.innerHeight;
-            const scrollY = window.scrollY;
-
-            return scrollY + viewportHeight >= beginRect.top + scrollY &&
-                   scrollY <= endRect.top + scrollY + endRect.height;
-        });
-
-        target.style.display = isVisible ? 'block' : 'none';
-    }
-
-    // Initialize the functionality
-    start(ele, areas) {
-        window.addEventListener('DOMContentLoaded', () => this.updateVisibility(ele, areas));
-        window.addEventListener('scroll', () => this.updateVisibility(ele, areas));
-    }
+  // Initialize the functionality
+  start(ele, areas) {
+    window.addEventListener('DOMContentLoaded', () => this.updateVisibility(ele, areas));
+    window.addEventListener('scroll', () => this.updateVisibility(ele, areas));
+  }
 }
 
 // export default Peekaboo;
